c16e74884c480c246768cc39731a01ad3d1435ae
dom alexander <dom.alexan@gmail.com>
2022-10-18 17:42:52 -0600

    commit tree 8e8a348236c9ff6f6f042c2d8a502307138826bb

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+shout("Hello!");
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+logShout("Hello");
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+logWhisper("Hello");
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!";
+    } 
+    else if (string === string.toUpperCase()) {
+        return "YES INDEED!" 
+    }
+    else if (string === "Let\'s have dinner together!") {
+        return "I would love to!"
+    }
+}
+
+sayHiToHeadphonedRoommate("Let\'s have dinner together!");
+
